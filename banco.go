ef9bc2eceb216e76f798120c7b0cd967f0af84f5
jhonatagd <88396979+jhonatagd@users.noreply.github.com>
2022-01-05 14:25:38 -0300

    Initial commit

---
+++ banco.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+type ContaCorrente struct {
+	titular       string
+	numeroAgencia int
+	numeroConta   int
+	saldo         float64
+}
+
+func main() {
+	contaDoMarquito := ContaCorrente{titular: "Guilherme",
+		numeroAgencia: 589, numeroConta: 123456, saldo: 125.5}
+
+	contaDoJp := ContaCorrente{"Jp", 222, 111222, 200}
+	contaDoDiegao := ContaCorrente{"Diegao", 222, 111222, 200}
+	contaDoAudi := ContaCorrente{"Audi", 222, 111222, 200}
+
+	fmt.Println(fmt.Sprintf("%v %v %v %v", contaDoMarquito, contaDoJp, contaDoDiegao, contaDoAudi))
+
+}
