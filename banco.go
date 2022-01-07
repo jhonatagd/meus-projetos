5b9663b0a1f82d1cfb673d567b170ac0f920c750
jhonatagd <88396979+jhonatagd@users.noreply.github.com>
2022-01-07 13:36:20 -0300

    banco.go

--- banco.go
+++ banco.go
@@ -9,13 +9,25 @@ type ContaCorrente struct {
 	saldo         float64
 }
 
+type contaDeTeste struct {
+	titular       string
+	numeroAgencia int
+	numeroConta   int
+	saldo         float64
+}
+
 func main() {
 	contaDoMarquito := ContaCorrente{titular: "Guilherme",
 		numeroAgencia: 589, numeroConta: 123456, saldo: 125.5}
 
-	contaDoJp := ContaCorrente{"Jp", 222, 111222, 200}
-	contaDoDiegao := ContaCorrente{"Diegao", 222, 111222, 200}
+	contaDeTeste := contaDeTeste{titular: "Vanessa",
+		numeroAgencia: 124, numeroConta: 193857, saldo: 153.5}
 
-	fmt.Println(fmt.Sprintf("%v %v %v", contaDoMarquito, contaDoJp, contaDoDiegao))
+	contaDoJp := ContaCorrente{"Jp", 220, 111220, 200}
+	contaDoDiegao := ContaCorrente{"Diegao", 221, 111221, 201}
+	contaDoAudi := ContaCorrente{"Audi", 222, 111222, 202}
+	contaDoGustavo := ContaCorrente{"Diegao", 223, 111223, 203}
+	contaDaVanessa := contaDeTeste{"Vanessa", 224, 111224, 204}
+	fmt.Println(fmt.Sprintf("%v %v %v %v %v %v", contaDoMarquito, contaDoJp, contaDoDiegao, contaDoAudi, contaDoGustavo, contaDaVanessa))
 
 }
