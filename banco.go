48f05bf4ffb2c8a23128b2e6cad9e6df80d45b75
jhonatagd <88396979+jhonatagd@users.noreply.github.com>
2022-01-07 14:12:51 -0300

    arrumei a string gustavo/diego

--- banco.go
+++ banco.go
@@ -26,7 +26,7 @@ func main() {
 	contaDoJp := ContaCorrente{"Jp", 220, 111220, 200}
 	contaDoDiegao := ContaCorrente{"Diegao", 221, 111221, 201}
 	contaDoAudi := ContaCorrente{"Audi", 222, 111222, 202}
-	contaDoGustavo := ContaCorrente{"Diegao", 223, 111223, 203}
+	contaDoGustavo := ContaCorrente{"Gustavo", 223, 111223, 203}
 	contaDaVanessa := contaDeTeste{"Vanessa", 224, 111224, 204}
 	fmt.Println(fmt.Sprintf("%v %v %v %v %v %v", contaDoMarquito, contaDoJp, contaDoDiegao, contaDoAudi, contaDoGustavo, contaDaVanessa))
 
